3a19b7bdb751f6250ea8fc53e3f79dfe710d299f
Katherine Wang <90437215+kcw27@users.noreply.github.com>
2025-08-02 09:43:31 -0400

    Update run_local_blast.sh
    Get information for all records clustered into each line of the BLAST output

--- scripts/blast_processing/run_local_blast.sh
+++ scripts/blast_processing/run_local_blast.sh
@@ -12,4 +12,4 @@
 # bash run_local_blast.sh /home/share/nr nr /home/kcw2/data/PAO1_PA3565.fasta /home/kcw2/data/PA3565_orthologs.txt &
 
 cd $1 # so that it can find the nr database
-blastp -db "$2" -query "$3" -max_target_seqs 500000 -outfmt "6 sgi sseqid sseq evalue stitle" -out $4
+blastp -db "$2" -query "$3" -max_target_seqs 500000 -outfmt "6 sallgi sallseqid sseq evalue salltitles" -out $4
