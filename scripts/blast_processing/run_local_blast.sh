f9f8e2fd6f17ac30eb591da5095b277c3abfc58a
Katherine Wang <kcw2@cepacia.bio.local.cmu.edu>
2025-08-04 10:25:11 -0400

    Update blast script

--- scripts/blast_processing/run_local_blast.sh
+++ scripts/blast_processing/run_local_blast.sh
@@ -9,7 +9,8 @@
 # Output: local blast of $3 against $2, saved to $4
 
 # Example run:
-# bash run_local_blast.sh /home/share/nr nr /home/kcw2/data/PAO1_PA3565.fasta /home/kcw2/data/PA3565_orthologs.txt &
+# bash /home/kcw2/ortholog-comparison-pipeline/scripts/blast_processing/run_local_blast.sh /home/share/pa/pa_db /home/kcw2/data/PAO1_PA3565.fasta $HOME/PA3565_orthologs_test.txt & # for other people to test
+# bash run_local_blast.sh /home/share/nr /home/kcw2/data/PAO1_PA3565.fasta /home/kcw2/data/PA3565_orthologs.txt &
 
-cd $1 # so that it can find the nr database
-blastp -db "$2" -query "$3" -max_target_seqs 500000 -outfmt "6 sgi sseqid sseq evalue stitle" -out $4
+cd $1 # so that it can find the blast database
+blastp -db "$2" -query "$3" -max_target_seqs 500000 -show_gis -num_threads 12 -outfmt "6 sallgi sallseqid sseq evalue salltitles" -out $4
