b08ae49a40c7f21593dacaf158ba9c6eff6529d6
Katherine Wang <kcw2@cepacia.bio.local.cmu.edu>
2025-08-05 12:27:22 -0400

    Add user execute permissions to scripts, update local blast to calculate nprocs

--- scripts/blast_processing/run_local_blast.sh
+++ scripts/blast_processing/run_local_blast.sh
@@ -9,9 +9,10 @@
 # Output: local blast of $3 against $2, saved to $4
 
 # Example run:
-# bash /home/kcw2/ortholog-comparison-pipeline/scripts/blast_processing/run_local_blast.sh /home/share/pa/pa_db pa /home/kcw2/data/PAO1_PA3565.fasta $HOME/PA3565_orthologs_test.txt & # for other people to test
+# bash /home/kcw2/ortholog-comparison-pipeline/scripts/blast_processing/run_local_blast.sh /home/share/pa/pa_db pa /home/kcw2/data/PAO1_PA3565.fasta $HOME/PA3565_orthologs_test.blast & # for other people to test
 # bash run_local_blast.sh /home/share/nr nr /home/kcw2/data/PAO1_PA3565.fasta /home/kcw2/data/PA3565_orthologs.txt &
 
 cd $1 # so that it can find the blast database
+procs_to_use=$(( $(nproc) / 4 ))
 echo "Running BLAST..."
-blastp -db "$2" -query "$3" -max_target_seqs 500000 -show_gis -num_threads 12 -outfmt "6 sallgi sallseqid sseq evalue salltitles" -out $4
+blastp -db "$2" -query "$3" -max_target_seqs 500000 -show_gis -num_threads $procs_to_use -outfmt "6 sallgi sallseqid sseq evalue salltitles" -out $4
